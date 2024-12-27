65a111f8a106811e3d319dd5d3451c0a060215b8
primi9 <thanprim@gmail.com>
2024-12-27 14:50:47 +0200

    change intro

--- js/onIntroLoad.js
+++ js/onIntroLoad.js
@@ -1,29 +1,28 @@
-//document.addEventListener("DOMContentLoaded", () => {
+const headerSection = document.getElementById("headerReveal");
+const gridElement = document.getElementById("imageGrid");
+const images = Array.from(document.querySelectorAll(".front-image")).reverse();
+const imgContainers = document.querySelectorAll(".img-container2");
+
+
 window.onload = function() {
-    
-    const headerSection = document.getElementById("headerReveal");
-    //const headerElement = document.getElementById("header");
-    //const enterButton = document.getElementById("enterButton");
-    const gridElement = document.getElementById("imageGrid");
-    const images = document.querySelectorAll(".to-reveal");
+   
+   const cells = gridElement.children;
+
+   images.forEach((image, index) => {
+      const currentCell = imgContainers[index];
+      const cellRect = currentCell.getBoundingClientRect();
+      const currentImagRect = image.getBoundingClientRect();
+
+      const translateX = cellRect.left - currentImagRect.left;
+      const translateY = cellRect.top - currentImagRect.top;
+      const scaleX = cellRect.width / currentImagRect.width;
+      const scaleY = cellRect.height / currentImagRect.height;
+
+      setTimeout(() => {
+         image.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scaleX}, ${scaleY})` ;
+      }, index * 500);
+
+   });
+   
  
-    images.forEach((image, index) => {
-          setTimeout(() => {
-          image.style.opacity = "1";
-          //image.style.transform = "translateY(0)";
-       }, index * 500);
-    });
-    
-    setTimeout(() => {
-       headerSection.classList.add('reveal-after');
-       //headerElement.classList.add('reveal-after');
-       //enterButton.style.display = "block";
-       //enterButton.style.opacity = "1";
-       //enterButton.classList.add('reveal-after-button');
-       //console.log(getComputedStyle(enterButton));
-       //background-color:rgba(0, 0, 0, 0.85);
-       //gridElement.style.background = "#fdf5e7";
-       
-       //gridElement.style.opacity = "0.3";
-    }, images.length * 500); 
-};   
+};
