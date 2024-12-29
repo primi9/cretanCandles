6fb5a53b33cb3c18232f0cea86d48f59534500e0
primi9 <thanprim@gmail.com>
2024-12-29 19:37:09 +0200

    Implement slideshow logic.

--- js/viewProduct.js
+++ js/viewProduct.js
@@ -162,31 +162,128 @@ modal.addEventListener('mouseup', mouseEnd);
 
 */
 
+function keyPressHandler(event) {
+
+    if(!focusedMode)
+        return;
+
+    if (event.key == "Escape"){
+        closeModal();
+        return;
+    }
+
+    if (event.key == "ArrowLeft"){
+        arrowPressed(-1);
+        return;
+    }
+
+    if (event.key == "ArrowRight"){
+        arrowPressed(1);
+        return;
+    }
+}
+
+function waitOpacityChange(opacityValue){
+
+    modal.style.opacity = opacityValue;
+
+    const promise = new Promise((resolve) => {
+        modal.addEventListener("transitionend", () => {
+            resolve();
+        }, {once: true});
+    });
+
+    return promise;
+}
+
+async function arrowPressed(n){
+    
+    if(processing)
+        return;
+
+    processing = true;
+
+    if (n == 1) {//if pressed next arrow
+        if (foregroundImageIndex == imageList.length - 1)
+            foregroundImageIndex = 0;
+        else 
+        foregroundImageIndex += 1;
+    }
+    else {
+        if (foregroundImageIndex == 0)
+            foregroundImageIndex = imageList.length - 1;
+        else 
+            foregroundImageIndex -= 1;
+    }
+
+    await waitOpacityChange("0");
+    displayFocusedImage(foregroundImageIndex);
+    await waitOpacityChange("1");
+
+    processing = false;
+}
+
+function displayFocusedImage(imgIndex) {
+    modal.src = imageList[imgIndex];
+}
+
+function focusProduct(){
+
+    focusedMode = true;
+
+    if (focusedIndex != -1){
+        imagesSlide[focusedIndex].classList.remove("active-slideShow-image");
+        focusedIndex = -1;
+    }
+
+    displayFocusedImage(foregroundImageIndex);
+
+    handleButtons.style.display = "block"; // display the handlers
+    modal.style.display = "flex"; // display the product in focused mode
+    document.body.style.overflowY = "hidden";//make the page un - scrollable when user is checking out the product
+}
+
+function closeModal() {
+
+    console.log("close button clicked");
+
+    if (foregroundImageIndex >= slideStartIndex && foregroundImageIndex < slideStartIndex + imagesSlide.length)
+        setFocusedImage(foregroundImageIndex - slideStartIndex);
+
+    handleButtons.style.display = "none";
+    
+    modal.style.display = "none";
+    document.body.style.overflowY = "auto";
+    
+    focusedMode = false;
+}
+
 function startSlider(){
     ArrowLeft.disabled = true;
+
     if (imageList.length - imagesSlide.length <= 0)
         ArrowRight.disabled = true;
 
     for(let i = 0; i < imagesSlide.length; i++)
         imagesSlide[i].src = imageList[i];
 
+    slideStartIndex = 0;
     focusedIndex = 0;
+    foregroundImageIndex = 0;
     imagesSlide[0].classList.add("active-slideShow-image");
     focusedImage.src = imageList[0];
 }
 
 function setFocusedImage(index){
 
-    if(focusedIndex == index)
-        return;
-
     if (focusedIndex != -1)
         imagesSlide[focusedIndex].classList.remove("active-slideShow-image");
 
     focusedIndex = index;
+    foregroundImageIndex = slideStartIndex + index;
 
     imagesSlide[focusedIndex].classList.add("active-slideShow-image");
-    focusedImage.src = imageList[focusedIndex + slideStartIndex];
+    focusedImage.src = imageList[foregroundImageIndex];
 }
 
 function leftArrowPressed(){
@@ -197,11 +294,20 @@ function leftArrowPressed(){
         imagesSlide[i].src = imageList[slideStartIndex + i];
 
     if(focusedIndex == imagesSlide.length - 1){
-        imagesSlide[imagesSlide.length - 1].classList.remove("active-slideShow-image");
+        imagesSlide[focusedIndex].classList.remove("active-slideShow-image");
         focusedIndex = -1;
     }
-    else if(focusedIndex != -1)
-        setFocusedImage(focusedIndex + 1);
+    else if(focusedIndex != -1){
+        imagesSlide[focusedIndex].classList.remove("active-slideShow-image");
+        imagesSlide[focusedIndex + 1].classList.add("active-slideShow-image");
+        focusedIndex += 1;
+    }
+    else if (focusedIndex == -1){
+        if (foregroundImageIndex == slideStartIndex){
+            focusedIndex = 0;
+            imagesSlide[0].classList.add("active-slideShow-image");
+        }
+    }
     
     ArrowRight.disabled = false;
     if(slideStartIndex == 0)
@@ -219,8 +325,17 @@ function rightArrowPressed(){
         imagesSlide[0].classList.remove("active-slideShow-image");
         focusedIndex = -1;
     }
-    else if(focusedIndex != -1)
-        setFocusedImage(focusedIndex - 1);
+    else if(focusedIndex != -1){
+        imagesSlide[focusedIndex].classList.remove("active-slideShow-image");
+        imagesSlide[focusedIndex - 1].classList.add("active-slideShow-image");
+        focusedIndex -= 1;
+    }
+    else if (focusedIndex == -1){
+        if (foregroundImageIndex == slideStartIndex + imagesSlide.length - 1){
+            focusedIndex = imagesSlide.length - 1;
+            imagesSlide[focusedIndex].classList.add("active-slideShow-image");
+        }
+    }
     
     ArrowLeft.disabled = false;
     if(slideStartIndex == imageList.length - imagesSlide.length)
@@ -233,15 +348,20 @@ const ArrowLeft = document.getElementById("leftArrow");
 const ArrowRight = document.getElementById("rightArrow");
 const imagesSlide = document.querySelectorAll(".slideImage");
 const imageList = ["images/eikona91.jpg" , "images/eikona92.jpg" , "images/eikona93.jpg" , "images/eikona94.jpg" , "images/eikona91.jpg","images/eikona91.jpg","images/eikona91.jpg"];
+const modal = document.getElementById('productModal');
 
-let slideStartIndex = 0;
+let slideStartIndex;
+let processing = false;
+let focusedMode = false;
 let focusedIndex;
+let foregroundImageIndex;
 
-startSlider();
-
+document.addEventListener('keydown', keyPressHandler);
 //set a listener
 imagesSlide.forEach((img, index) => {
     img.addEventListener('click', () => {
         setFocusedImage(index);
     });
 });
+
+startSlider();
