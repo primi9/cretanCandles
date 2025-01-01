32ec805005d94541c9a9c62bf249c6f35c485288
primi9 <thanprim@gmail.com>
2025-01-01 12:45:57 +0200

    Display none when user scrolls

--- js/mainInfo.js
+++ js/mainInfo.js
@@ -5,11 +5,15 @@ window.addEventListener('scroll', function() {
     const infoTopPos = mainInfo.getBoundingClientRect().top;
     if (infoTopPos <= 0){
         mainInfo.children[0].style.display = "block";
+        mainInfo.children[3].style.display = "none";
+        mainInfo.children[4].style.display = "none";
         mainInfo.style.justifyContent = "flex-end";
     }
     else {
         mainInfo.style.justifyContent = "center";
         mainInfo.children[0].style.display = "none";
+        mainInfo.children[3].style.display = "inline-block";
+        mainInfo.children[4].style.display = "inline-block";
     }
 
 });
