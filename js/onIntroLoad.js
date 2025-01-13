3d898014bd148f228b0026a80fb02e6a6d0fef1a
primi9 <thanprim@gmail.com>
2025-01-13 14:01:28 +0200

    Change intro.

--- js/onIntroLoad.js
+++ js/onIntroLoad.js
@@ -2,28 +2,17 @@
 window.onload = function() {
     
     const headerSection = document.getElementById("headerReveal");
-    //const headerElement = document.getElementById("header");
-    //const enterButton = document.getElementById("enterButton");
+
     const gridElement = document.getElementById("imageGrid");
     const images = document.querySelectorAll(".to-reveal");
  
     images.forEach((image, index) => {
           setTimeout(() => {
            image.style.transform = "scale(1)";
-          //image.style.transform = "translateY(0)";
-       }, index * 600);
+       }, index * 800);
     });
     
     setTimeout(() => {
        headerSection.classList.add('reveal-after');
-       //headerElement.classList.add('reveal-after');
-       //enterButton.style.display = "block";
-       //enterButton.style.opacity = "1";
-       //enterButton.classList.add('reveal-after-button');
-       //console.log(getComputedStyle(enterButton));
-       //background-color:rgba(0, 0, 0, 0.85);
-       //gridElement.style.background = "#fdf5e7";
-       
-       //gridElement.style.opacity = "0.3";
-    }, images.length * 650); 
+    }, images.length * 800); 
 };   
