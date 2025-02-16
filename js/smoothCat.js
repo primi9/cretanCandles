b501a2ca51d72f619077ab96abd7b5f5d75acdad
primi9 <thanprim@gmail.com>
2025-02-16 12:06:55 +0200

    on load content

--- js/smoothCat.js
+++ js/smoothCat.js
@@ -14,24 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     categories.forEach(category => observer.observe(category));
+
+    const mainSlideImages = ["images/candles1.png" , "images/candles2.png" , "images/candles3.png" , "images/candles4.png"];
+    const backgroundImg = document.getElementById("backgroundImg");
+    const numImages = mainSlideImages.length;
+    let index = 0;
+
+    function changeSlideBackground() {
+        
+        backgroundImg.style.backgroundImage = `url('${mainSlideImages[index]}')`;
+
+        if(index % 2)
+            backgroundImg.style.transform = "scale(1)";
+        else
+            backgroundImg.style.transform = "scale(1.05)";
+
+        index = (index + 1) % numImages; 
+    }
+
+    changeSlideBackground();
+    setInterval(changeSlideBackground, 3500);
 });
-
-const mainSlideImages = ["images/candles1.png" , "images/candles2.png" , "images/candles3.png" , "images/candles4.png"];
-const backgroundImg = document.getElementById("backgroundImg");
-const numImages = mainSlideImages.length;
-let index = 0;
-
-function changeSlideBackground() {
-    
-    backgroundImg.style.backgroundImage = `url('${mainSlideImages[index]}')`;
-
-    if(index % 2)
-        backgroundImg.style.transform = "scale(1)";
-    else
-        backgroundImg.style.transform = "scale(1.05)";
-
-    index = (index + 1) % numImages; 
-}
-
-changeSlideBackground();
-setInterval(changeSlideBackground, 3500);
