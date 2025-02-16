89cfead90f1d25f73529af504573bacf2c1e7032
primi9 <thanprim@gmail.com>
2025-02-16 16:15:06 +0200

    change layout

--- js/smoothCat.js
+++ js/smoothCat.js
@@ -14,24 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     categories.forEach(category => observer.observe(category));
+});
 
-    const mainSlideImages = ["images/candles1.webp" , "images/candles2.webp" , "images/candles3.webp" , "images/candles4.webp"];
-    const backgroundImg = document.getElementById("backgroundImg");
-    const numImages = mainSlideImages.length;
-    let index = 0;
+
+
+window.onload = function () {
 
     function changeSlideBackground() {
         
         backgroundImg.style.backgroundImage = `url('${mainSlideImages[index]}')`;
-
+    
         if(index % 2)
             backgroundImg.style.transform = "scale(1)";
         else
             backgroundImg.style.transform = "scale(1.05)";
-
+    
         index = (index + 1) % numImages; 
     }
+    
+    const mainSlideImages = ["images/candles1.webp" , "images/candles2.webp" , "images/candles3.webp" , "images/candles4.webp"];
 
+    const backgroundImg = document.getElementById("backgroundImg");
+    const numImages = mainSlideImages.length;
+
+    let index = 0;
+    
     changeSlideBackground();
     setInterval(changeSlideBackground, 3500);
-});
+};
